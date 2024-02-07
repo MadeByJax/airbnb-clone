e83ce146cf0a1491c959205446c382bf6bde4b41
MadeByJax <jaxzynfollett@outlook.com>
2024-02-07 16:42:31 +1100

    Refactor MenuItem component

--- app/components/navbar/MenuItem.tsx
+++ app/components/navbar/MenuItem.tsx
@@ -1,28 +1,27 @@
 "use client";
+import React, { ReactNode } from "react";
 
 interface MenuItemProps {
-    onClick: () => void;
-    label: string;
-
+  onClick: () => void;
+  label: ReactNode;
 }
-    
 
-const MenuItem: React.FC<MenuItemProps> = ({
-    onClick,
-    label
-}) => {
+const MenuItem: React.FC<MenuItemProps> = ({ onClick, label }) => {
   return (
-    <div 
-    onClick={onClick}
-    className=" 
+    <div
+      onClick={onClick}
+      className=" 
     px-4
     py-3
     hover:bg-neutral-100
     transition
     font-semibold
 
-    ">{label}</div>
-  )
-}
+    "
+    >
+      {label}
+    </div>
+  );
+};
 
-export default MenuItem
+export default MenuItem;
