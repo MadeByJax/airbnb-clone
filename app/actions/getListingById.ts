23bffad2b57de99c5399e105a87b8c04b64ff67b
MadeByJax <jaxzynfollett@outlook.com>
2024-03-01 21:04:12 +1100

    Add getListingById action

---
+++ app/actions/getListingById.ts
@@ -0,0 +1,37 @@
+import prisma from "@/app/libs/prismadb";
+
+interface IParams {
+  listingId: string; // You need to specify a type for listingId
+}
+
+export default async function getListingById(params: IParams) {
+  try {
+    const { listingId } = params;
+
+    const listing = await prisma.listing.findUnique({
+      where: {
+        id: listingId,
+      },
+      include: {
+        user: true,
+      },
+    });
+
+    if (!listing) {
+      return null;
+    }
+    return {
+      ...listing,
+      createdAt: listing.createdAt.toISOString(),
+      user: {
+        ...listing.user,
+        createdAt: listing.user.createdAt.toISOString(),
+        updatedAt: listing.user.updatedAt.toISOString(),
+        emailVerified: listing.user.emailVerified?.toISOString() || null,
+      },
+    };
+  } catch (error: any) {
+    console.error(error);
+    throw error;
+  }
+}
